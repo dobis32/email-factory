e7e2a412bc4fc6de18afeda8143200b4c019d440
dobis <scottvanderweide32@gmail.com>
2021-06-24 07:35:20 -0400

    working on spitting out html

--- src/classes/SupportedHTMLElement.ts
+++ src/classes/SupportedHTMLElement.ts
@@ -1,3 +1,5 @@
+import iHTMLAttribute from "@/interfaces/iHTMLAttribute";
+
 export default class SupportedHTMLElement {
     private _validChildren: Array<string>;
     private _elementName: string;
@@ -18,4 +20,20 @@ export default class SupportedHTMLElement {
         return this._validChildren.find((el: string) => el == target) ? true : false;
     }
 
+    getHTML(attributes: Array<iHTMLAttribute>): string {
+        // TODO unit test; unit test the element-text functionality too
+        const textContentAttribute = 'element-text'
+        const elementText = attributes.find((att: iHTMLAttribute) => att.name === textContentAttribute) as iHTMLAttribute;
+        const inlineAttributes = attributes.filter((att: iHTMLAttribute) => att.name !== textContentAttribute) as Array<iHTMLAttribute>;
+        let output = '';
+
+        output += `<${this._elementName}`
+        attributes.forEach((att: iHTMLAttribute) => {
+            if (att.name !== 'element-text') output +=  ` ${att.name}="${att.value}"`
+        });
+        output += '>'
+        output += `${elementText ? elementText.value : ''}`
+        output += ` </${this._elementName}>`;
+        return output;
+    }
 }
